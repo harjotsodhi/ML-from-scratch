6de8272be93afb1bb0e129bf5a8104bb3f691814
harjotsodhi <61566074+harjotsodhi@users.noreply.github.com>
2021-10-13 13:19:32 -0400

    Implement and test random forest.

--- mlfromscratch/supervised/random_forest.py
+++ mlfromscratch/supervised/random_forest.py
@@ -1,1 +1,88 @@
 import numpy as np
+from ..helpers import helpers as hp
+from .cart import Classification_tree
+
+
+
+class Random_forest(object):
+    """
+    Implements the Random Forest ensemble method for training
+        "num_trees" decision trees on sub-samples of the data.
+
+    Parameters
+    ----------
+    num_trees: int, default=10
+        number of trees in the forest.
+
+    num_samples: int, default=None
+        number of samples to draw (with replacement)
+        for each bootstrapped dataset.
+        If None then all samples are used.
+
+    seed: int, default=17
+        controls the randomness of the bootstrapping.
+    """
+    def __init__(self, num_trees=10, num_samples=None, seed=17):
+        self.trees = [Classification_tree() for n in range(num_trees)]
+        self.num_samples = num_samples
+        self.seed = seed
+
+
+    def fit(self, X, y):
+        """
+        Fit "num_trees" classification trees using
+            bootstrapped samples of size "num_samples"
+
+        Parameters
+        ----------
+        X: np.array
+            feature matrix (m x n)
+
+        y: np.array
+            response vector (m x 1)
+        """
+        for t in self.trees:
+            bootstrap_ind = self._bootstrap(X)
+            t.fit(X[bootstrap_ind], y[bootstrap_ind])
+
+
+    def predict(self, X):
+        '''
+        Return the predicted value.
+
+        Parameters
+        ----------
+        X: np.array
+            feature matrix (m x n)
+        '''
+        y_pred_all = np.zeros((X.shape[0], len(self.trees)))
+        y_pred = np.zeros(X.shape[0])
+
+        for c in range(len(self.trees)):
+            y_pred_all[:, c] = self.trees[c].predict(X)
+
+        for r in range(X.shape[0]):
+            y_pred[r] = hp.mode(y_pred_all[r,:])
+
+        return y_pred
+
+
+    ### Private Methods ###
+
+    def _bootstrap(self, X):
+        '''
+        Random sampling of feature matrix X
+            with replacement
+
+        Parameters
+        ----------
+        X: np.array
+            feature matrix (m x n)
+        '''
+        np.random.seed(self.seed)
+        
+        if not self.num_samples:
+            self.num_samples = X.shape[0]
+
+        ind = np.random.choice(X.shape[0], self.num_samples, replace=True)
+        return ind
---
+++ mlfromscratch/testing/test_random_forest.py
@@ -0,0 +1,31 @@
+from ..supervised.random_forest import Random_forest
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+
+def test():
+    iris = load_iris()
+    X = iris['data']
+    y = iris['target']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+    # custom implementation
+    rf = Random_forest()
+    rf.fit(X_train,y_train)
+    y_pred = rf.predict(X_test)
+    accuracy = (y_test == y_pred).sum()/y_test.shape[0]
+
+    # sklearn implementation
+    rf_sklearn = RandomForestClassifier(n_estimators=10, criterion="entropy", random_state=0)
+    rf_sklearn.fit(X_train,y_train)
+    y_pred_sklearn = rf_sklearn.predict(X_test)
+    accuracy_sklearn = (y_test == y_pred_sklearn).sum()/y_test.shape[0]
+
+    # test
+    np.testing.assert_allclose(accuracy, accuracy_sklearn)
+
+
+if __name__ == '__main__':
+    test()
