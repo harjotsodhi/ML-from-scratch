42d034dc06fe7c0bfc6c25191c6c91b46d839480
harjotsodhi <61566074+harjotsodhi@users.noreply.github.com>
2021-10-26 11:51:41 -0400

    Implement and test PCA.

---
+++ mlfromscratch/testing/test_pca.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn import decomposition
+from sklearn.datasets import load_iris
+from ..unsupervised.pca import PCA
+from ..helpers import helpers as hp
+
+
+def test():
+
+    iris = load_iris()
+    X = iris.data
+
+    pca = PCA(n_components=2)
+    pca.fit(X)
+    X_reduced = pca.project(X)
+
+    pca_sklearn = decomposition.PCA(n_components=2)
+    X = hp.standardize(X)
+    pca_sklearn.fit(X)
+    X_reduced_sklearn = pca_sklearn.transform(X)
+
+    # check if projections are equal
+    # the sign between sklearn's projections may be swapped, so just ignore
+    np.testing.assert_allclose(abs(X_reduced), abs(X_reduced_sklearn))
+
+
+if __name__ == '__main__':
+    test()
--- mlfromscratch/unsupervised/pca.py
+++ mlfromscratch/unsupervised/pca.py
@@ -1,1 +1,76 @@
 import numpy as np
+from ..helpers import helpers as hp
+
+
+
+class PCA(object):
+
+    def __init__(self, n_components=2):
+        '''
+        Implements the PCA algorithm.
+
+        Parameters
+        ----------
+        n_components: Number of principal components
+        '''
+        self.n_components = n_components
+
+
+    def fit(self, X):
+        '''
+        Calculate the first n_components eigenvectors
+
+        Parameters
+        ----------
+        X: np.array
+            feature matrix (m x n)
+        '''
+        X_scaled = hp.standardize(X)
+        cov_mat = self._covariance(X_scaled)
+        eig_vals, eig_vects = self._eigen_decomp(cov_mat)
+        # sort the eigenvectors by their respective eigenvalues
+        order = np.argsort(eig_vals)[::-1]
+        self.eig_vects = eig_vects[:,order]
+
+
+    def project(self, X):
+        '''
+        Project data on to n_components principal components
+
+        Parameters
+        ----------
+        X: np.array
+            feature matrix (m x n)
+        '''
+        X_scaled = hp.standardize(X)
+        x_project = X_scaled.dot(self.eig_vects[:,:self.n_components])
+        return x_project
+
+
+    ## private methods ##
+
+
+    def _covariance(self, X):
+        '''
+        Compute the covariance (n x n) matrix.
+
+        Parameters
+        ----------
+        X: np.array
+            feature matrix (m x n)
+        '''
+        return np.cov(X.T)
+
+
+    def _eigen_decomp(self, X):
+        '''
+        Decomposes a matrix into eigenvectors and eigenvalues.
+
+        Parameters
+        ----------
+        X: np.array
+            feature matrix (n x n)
+        '''
+        assert X.shape[0] == X.shape[1], "X must be a square matrix"
+        eig_vals, eig_vects = np.linalg.eig(X)
+        return eig_vals, eig_vects
